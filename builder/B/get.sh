b15c5670c2a4461f102b6ecd49ed0ca5da456414
wryyyyyyyy <no@spam.here>
2021-01-24 12:22:46 +0100

    minor fix

---
+++ builder/B/get.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#wget -c "https://www.lua.org/ftp/lua-5.4.2.tar.gz"
+#wget -c "https://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz"
+#wget -c "https://www.cpan.org/src/5.0/perl-5.32.1.tar.gz"
+#wget -c "ftp://ftp.lysator.liu.se/pub/unix/pnscan/pnscan-1.11.tar.gz"
+#wget -c "https://gist.githubusercontent.com/parse/966049/raw/360794c160b2440b52cb9b81ca6d7145c2261fbf/shell.c"
---
+++ builder/B/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export IMG="pnscan"
+export TAG="latest"
+export REPO="docker"
+export REGISTRY="ghcr.io"
