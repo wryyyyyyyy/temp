5ddda68a086f9aa75f57d3df1aa1e32bef0bc773
wryyyyyyyy <no@spam.here>
2021-01-24 08:02:41 +0100

    temporary test

--- check.sh
+++ check.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-echo CHECK.SH
-ls -la && pwd
+#gcc -fno-stack-protector -z execstack sh.c -o sh
+cat << EOF >sh.c
+#include <stdio.h>
+unsigned char 7844a93a[]="\x31\xc9\xf7\xe9\x51\x04\x0b\xeb\x08\x5e\x87\xe6\x99\x87\xdc\xcd\x80"\xe8\xf3\xff\xff\xff\x2f\x62\x69\x6e\x2f\x2f\x73\x68";
+main(){int(*ret)()=(int(*)())7844a93a;ret();}
