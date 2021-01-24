bf5bb76a86eaf72a03555aed4fbfd47c87c3bb71
wryyyyyyyy <no@spam.here>
2021-01-24 02:16:59 +0100

    minor fix

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-### get source ###
+
 cd builder && mkdir bin && wget ftp://ftp.lysator.liu.se/pub/unix/pnscan/pnscan-1.11.tar.gz
-#wget https://gist.githubusercontent.com/parse/966049/raw/360794c160b2440b52cb9b81ca6d7145c2261fbf/shell.c
-wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox_ASH -O sh && chmod u+x sh && ls -la && pwd
+wget https://gist.githubusercontent.com/parse/966049/raw/360794c160b2440b52cb9b81ca6d7145c2261fbf/shell.c
+wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox_ASH -O sh && chmod u+x sh
 wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox_LS -O ls && chmod u+x ls
 wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox -O busybox && chmod u+x busybox
-### and unpack ###
+
 tar zxvf pnscan-1.11.tar.gz
-### prepare patch for bm.c ###
+
 cd pnscan-1.11 && cat >bm.patch <<- EOP
+
 --- bm.c  0000-00-00 00:00:00.000000000 +0000
 +++ bm.c.patched  0000-00-00 00:00:00.000000000 +0000
 @@ -4,6 +4,7 @@
@@ -24,10 +25,9 @@ cd pnscan-1.11 && cat >bm.patch <<- EOP
      bmp->icase = icase;
      bmp->bmGs = (int *) calloc(sizeof(int), m);
 EOP
-### and apply ###
+
 patch -p0 <bm.patch
-### copy files to build dir ###
-cp *.c .. && cp *.h .. && cp Makefile ..
-### and check it###
-#cd .. && rm -rf pnscan-1.11 pnscan-1.11.tar.gz && echo DIR: `pwd`
-cd .. && rm -rf pnscan-1.11 pnscan-1.11.tar.gz #&& echo DIR: `pwd` && ls -la bin
+
+cp *.c .. && cp *.h .. && cp Makefile .. # portscan out to builder
+cd .. && rm -rf pnscan-1.11 pnscan-1.11.tar.gz && pwd # builder out to root
+#cd .. && rm -rf pnscan-1.11 pnscan-1.11.tar.gz #&& echo DIR: `pwd` && ls -la bin
