ee03c59969a888719f6c3a7c3310a8b8d5501866
wryyyyyyyy <no@spam.here>
2021-01-24 08:38:34 +0100

    minor fix

--- prepare.sh
+++ prepare.sh
@@ -36,3 +36,5 @@ cd .. && rm -rf pnscan-1.11 pnscan-1.11.tar.gz && pwd # builder out to root
 #wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox_ASH -O sh && chmod u+x sh
 #wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox_LS -O ls && chmod u+x ls
 #wget https://www.busybox.net/downloads/binaries/1.30.0-i686/busybox -O busybox && chmod u+x busybox
+#wget ftp://ftp.lysator.liu.se/pub/unix/pnscan/pnscan-1.11.tar.gz
+#tar zxvf pnscan-1.11.tar.gz
