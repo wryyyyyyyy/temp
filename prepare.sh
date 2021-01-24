52516916747a184124e6e3727585d31ced6b9a8a
wryyyyyyyy <no@spam.here>
2021-01-24 08:26:01 +0100

    minor fix

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd builder && ls -la
+cd builder && mkdir B && ls -la
 wget "ftp://ftp.lysator.liu.se/pub/unix/pnscan/pnscan-1.11.tar.gz"
 wget "https://gist.githubusercontent.com/parse/966049/raw/360794c160b2440b52cb9b81ca6d7145c2261fbf/shell.c"
 
